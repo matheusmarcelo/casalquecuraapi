7b29ee0fd31e0260736285aa0769087ded1c0a29
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-27 20:06:07 -0300

    feature: create the customer use cases

--- src/modules/customer/customer.service.ts
+++ src/modules/customer/customer.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import type { ICustomerRepository } from 'src/constants/contracts/customer/ICustomerRepository.contract';
+import { ICustomerService } from 'src/constants/contracts/customer/ICustomerService.contract';
+import { DITokens } from 'src/constants/enums/DITokens/DITokens.enum';
+import { CustomerDto } from 'src/dtos/customer/customer.dto';
+import { FindCustomersDto } from 'src/dtos/customer/findCustomers.dto';
+import { Customer } from 'src/entitites/customer/customer.entity';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService implements ICustomerService {
+
+    constructor(
+        @Inject(DITokens.CUSTOMER_REPOSITORY)
+        private readonly customerRepository: ICustomerRepository
+    ) { }
+
+    async createCustomerAsync(customerDto: CustomerDto): Promise<void> {
+        const customer = await this.customerRepository.getCustomerByEmail(customerDto.email);
+
+        if (customer) {
+            throw new HttpException('Customer alredy created', HttpStatus.BAD_REQUEST);
+        }
+
+        await this.customerRepository.createCustomerAsync(customerDto);
+    }
+
+    async getCustomerByIdAsync(id: string): Promise<Customer | null> {
+        const customer = await this.customerRepository.getCustomerByIdAsync(id);
+
+        if (!customer) {
+            throw new HttpException('Customer not found', HttpStatus.NOT_FOUND);
+        }
+
+        return customer;
+    }
+
+    async getCustomersAsync(params?: FindCustomersDto): Promise<Customer[]> {
+        return await this.customerRepository.getCustomersAsync(params);
+    }
+
+    async updateCustomerAsync(id: string, customer: Customer): Promise<void> {
+        await this.customerRepository.updateCustomerAsync(id, customer);
+    }
+
+    async disableCustomerAsync(id: string): Promise<void> {
+        await this.disableCustomerAsync(id);
+    }
+}
