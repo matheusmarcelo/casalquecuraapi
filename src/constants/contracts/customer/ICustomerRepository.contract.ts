63217fe361aa2ab6b45006a256c8d4aca7fc8274
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-27 20:04:51 -0300

    feature: create the contracts for customer repository and service

--- src/constants/contracts/customer/ICustomerRepository.contract.ts
+++ src/constants/contracts/customer/ICustomerRepository.contract.ts
@@ -1,8 +1,9 @@
+import { CustomerDto } from "src/dtos/customer/customer.dto";
 import { FindCustomersDto } from "src/dtos/customer/findCustomers.dto";
 import { Customer } from "src/entitites/customer/customer.entity";
 
 export interface ICustomerRepository {
-    createCustomerAsync(customer: Customer): Promise<void>;
+    createCustomerAsync(customer: CustomerDto): Promise<void>;
     getCustomerByIdAsync(id: string): Promise<Customer | null>;
     getCustomersAsync(params?: FindCustomersDto): Promise<Customer[]>;
     updateCustomerAsync(id: string, customer: Customer): Promise<void>;
---
+++ src/constants/contracts/customer/ICustomerService.contract.ts
@@ -0,0 +1,11 @@
+import { CustomerDto } from "src/dtos/customer/customer.dto";
+import { FindCustomersDto } from "src/dtos/customer/findCustomers.dto";
+import { Customer } from "src/entitites/customer/customer.entity";
+
+export interface ICustomerService {
+    createCustomerAsync(customerDto: CustomerDto): Promise<void>;
+    getCustomerByIdAsync(id: string): Promise<Customer | null>;
+    getCustomersAsync(params?: FindCustomersDto): Promise<Customer[]>;
+    updateCustomerAsync(id: string, customer: Customer): Promise<void>;
+    disableCustomerAsync(id: string): Promise<void>;
+}
