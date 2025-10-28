88e8448a86f989cbdff2a28be908994a8fbe0a1b
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-28 17:49:45 -0300

    refactor: Change the method for validation string length

--- src/dtos/activity/activity.dto.ts
+++ src/dtos/activity/activity.dto.ts
@@ -1,10 +1,10 @@
 import { Type } from "class-transformer";
-import { IsBoolean, IsDecimal, IsNotEmpty, IsNumber, IsOptional, IsString, Max, MaxLength, Min } from "class-validator";
+import { IsBoolean, IsDecimal, IsNotEmpty, IsNumber, IsOptional, IsString, Max, MaxLength, Min, MinLength } from "class-validator";
 
 export class ActivityDto {
     @IsString({ message: 'Title must be a string type' })
     @MaxLength(100, { message: 'Max 100 character for title' })
-    @MaxLength(3, { message: 'Min 3 character for title' })
+    @MinLength(3, { message: 'Min 3 character for title' })
     @IsNotEmpty({ message: 'Title must have a value' })
     title: string;
 
@@ -15,8 +15,8 @@ export class ActivityDto {
 
     @IsNumber({}, { message: 'Score must be a number type' })
     @Type(() => Number)
-    @Min(1, { message: 'Min 1 point for score' })
-    @Max(100, { message: 'Max 100 point for score' })
+    @MinLength(1, { message: 'Min 1 point for score' })
+    @MaxLength(100, { message: 'Max 100 point for score' })
     score: string;
 
     @IsBoolean({ message: 'isGeneral must be a boolean type' })
