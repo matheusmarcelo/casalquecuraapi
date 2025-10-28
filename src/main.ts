ee765fba24dbb24c0e587a523ef199947610506b
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-28 17:36:07 -0300

    feature: configure the app for global excepiton filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './common/filters/global-exception/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
