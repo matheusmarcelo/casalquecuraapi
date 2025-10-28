d34844d30416589254234d76f9a5a53872956a5b
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-28 17:08:41 -0300

    feature: create the valdations for activity dto

---
+++ src/dtos/activity/activity.dto.ts
@@ -0,0 +1,25 @@
+import { Type } from "class-transformer";
+import { IsBoolean, IsDecimal, IsNotEmpty, IsNumber, IsOptional, IsString, Max, MaxLength, Min } from "class-validator";
+
+export class ActivityDto {
+    @IsString({ message: 'Title must be a string type' })
+    @MaxLength(100, { message: 'Max 100 character for title' })
+    @MaxLength(3, { message: 'Min 3 character for title' })
+    @IsNotEmpty({ message: 'Title must have a value' })
+    title: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Description must have a value' })
+    @MaxLength(3, { message: 'Min 3 character for title' })
+    description: string;
+
+    @IsNumber({}, { message: 'Score must be a number type' })
+    @Type(() => Number)
+    @Min(1, { message: 'Min 1 point for score' })
+    @Max(100, { message: 'Max 100 point for score' })
+    score: string;
+
+    @IsBoolean({ message: 'isGeneral must be a boolean type' })
+    @IsOptional()
+    isGeneral: string;
+}
