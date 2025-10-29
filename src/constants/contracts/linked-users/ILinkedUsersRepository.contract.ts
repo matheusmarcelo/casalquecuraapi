ae6c0dcb236612b0f64f7aa37d5347b9e3b8f713
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-29 20:59:58 -0300

    feature: create contract for linke users repository

---
+++ src/constants/contracts/linked-users/ILinkedUsersRepository.contract.ts
@@ -0,0 +1,12 @@
+import { AuxLinkedUsers } from "src/entitites/linked-users/aux_linked_users.entity";
+import { LinkedUsers } from "src/entitites/linked-users/linked_users.entity";
+
+export interface ILinkedUsersRepository {
+    createLinkedUsersTemporaryAsync(linkedUsers: AuxLinkedUsers): Promise<void>;
+    createLinkedUsersAsync(linkedUsers: LinkedUsers): Promise<void>;
+    deleteLinkedUsersTemporaryAsync(id: string): Promise<void>;
+    deleteLinkedUsersAsync(id: string): Promise<void>;
+    getLinkedUsersTemporaryAsync(customerId: string): Promise<AuxLinkedUsers | null>;
+    getLinkedUsersAsync(customerId: string): Promise<LinkedUsers | null>;
+    getLinkedUsersByCustomersIdAsync(fromId: string, toId: string): Promise<LinkedUsers | null>;
+}
