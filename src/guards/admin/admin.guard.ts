8d5403fad47eef55b3ce5be25b131a34c4f2a92d
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-30 20:00:37 -0300

    feature: create the rules for admin access

---
+++ src/guards/admin/admin.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserRoles } from 'src/constants/enums/userRole.enum';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (user?.role !== UserRoles.ADMIN) {
+      throw new ForbiddenException('Access denied. Administrators only.');
+    }
+
+    return true;
+  }
+}
