9c17c163ef1d1951ad205b6a8a89acb40ee650cf
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-29 22:58:11 -0300

    refactor: omit getLinkedUsersSolicitationAsync method

--- src/constants/contracts/linked-users/ILinkedUsersService.contract.ts
+++ src/constants/contracts/linked-users/ILinkedUsersService.contract.ts
@@ -1,9 +1,9 @@
 import { LinkedUsersDto } from "src/dtos/linked_users/linkedUsers.dto";
 import { ILinkedUsersRepository } from "./ILinkedUsersRepository.contract";
 
-export interface ILinkedUsersService extends Omit<ILinkedUsersRepository, 'getLinkedUsersByCustomersIdAsync' | 'createLinkedUsersTemporaryAsync' | 'createLinkedUsersAsync' | 'deleteLinkedUsersTemporaryAsync' | 'deleteLinkedUsersAsync'> {
+export interface ILinkedUsersService extends Omit<ILinkedUsersRepository, 'getLinkedUsersByCustomersIdAsync' | 'createLinkedUsersTemporaryAsync' | 'createLinkedUsersAsync' | 'deleteLinkedUsersTemporaryAsync' | 'deleteLinkedUsersAsync' | 'getLinkedUsersSolicitationAsync'> {
     createLinkedUsersTemporaryAsync(linkedUsers: LinkedUsersDto): Promise<void>;
-    createLinkedUsersAsync(linkedUsers: LinkedUsersDto): Promise<void>;
+    createLinkedUsersAsync(solicitationId: string): Promise<void>;
     deleteLinkedUsersTemporaryAsync(customerId: string): Promise<void>;
     deleteLinkedUsersAsync(customerId: string): Promise<void>;
 }
