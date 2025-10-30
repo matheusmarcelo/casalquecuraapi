5553bb3fd9438f5297c1959f113f43dc0aa16e86
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-30 20:02:56 -0300

    refactor: add authGuard and adminGuard for requests

--- src/modules/customer_activity/customer_activity.controller.ts
+++ src/modules/customer_activity/customer_activity.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Inject, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Inject, Param, Post, UseGuards } from '@nestjs/common';
 import type { ICustomerActivityService } from 'src/constants/contracts/customer-activity/ICustomerActivitiesService.contract';
 import { DITokensService } from 'src/constants/enums/DITokens/DITokens.enum';
 import { CustomerActivityDto } from 'src/dtos/customer_activity/customerActivity.dto';
 import { CustomerActivity } from 'src/entitites/customer-activity/customer-activity.entity';
+import { AdminGuard } from 'src/guards/admin/admin.guard';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
 
 @Controller('customer/activities')
 export class CustomerActivityController {
@@ -12,11 +14,13 @@ export class CustomerActivityController {
   ) { }
 
   @Post('')
+  @UseGuards(AuthGuard, AdminGuard)
   async createCustomerActivityAsync(@Body() customer_activity: CustomerActivityDto): Promise<void> {
     await this.customerActivityService.createCustomerActivityAsync(customer_activity);
   }
 
   @Get('/:customerId')
+  @UseGuards(AuthGuard)
   async getCustomerActivitiesAsync(@Param('customerId') customerId: string): Promise<CustomerActivity[]> {
     const customerActivities = await this.customerActivityService.getCustomerActivitiesAsync(customerId);
     return customerActivities;
@@ -24,6 +28,7 @@ export class CustomerActivityController {
 
   @Delete('/:id')
   @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard, AdminGuard)
   async deleteCustomerActivityAsync(@Param('id') id: string): Promise<void> {
     await this.customerActivityService.deleteCustomerActivityAsync(id);
   }
