78a5924cf6d3a87436c902d9e858def402e8580a
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-29 21:34:15 -0300

    refactor: add the DIToken for Linked Users Repository

--- src/constants/enums/DITokens/DITokens.enum.ts
+++ src/constants/enums/DITokens/DITokens.enum.ts
@@ -2,6 +2,7 @@ export enum DITokensRepository {
     CUSTOMER_REPOSITORY = 'ICustomerRepository',
     ACTIVITY_REPOSITORY = 'IActivityRepository',
     CUSTOMER_ACTIVITY_REPOSITORY = 'ICustomerActivityRepository',
+    LINKED_USERS_REPOSITORY = 'ILinkedUsersRepository',
 }
 
 export enum DITokensService {
