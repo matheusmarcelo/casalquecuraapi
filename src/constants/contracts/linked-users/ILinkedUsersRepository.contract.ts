06e95720797d04c5898a3d9a500c5792bf6c48ee
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-29 21:33:38 -0300

    refactor: change the path for import

--- src/constants/contracts/linked-users/ILinkedUsersRepository.contract.ts
+++ src/constants/contracts/linked-users/ILinkedUsersRepository.contract.ts
@@ -1,3 +1,4 @@
+import { LinkedUsersDto } from "src/dtos/linked_users/linkedUsers.dto";
 import { AuxLinkedUsers } from "src/entitites/linked-users/aux_linked_users.entity";
 import { LinkedUsers } from "src/entitites/linked-users/linked_users.entity";
 
@@ -6,7 +7,7 @@ export interface ILinkedUsersRepository {
     createLinkedUsersAsync(linkedUsers: LinkedUsers): Promise<void>;
     deleteLinkedUsersTemporaryAsync(id: string): Promise<void>;
     deleteLinkedUsersAsync(id: string): Promise<void>;
-    getLinkedUsersTemporaryAsync(customerId: string): Promise<AuxLinkedUsers | null>;
+    getLinkedUsersTemporaryAsync(customerId: string): Promise<LinkedUsersDto | null>;
     getLinkedUsersAsync(customerId: string): Promise<LinkedUsers | null>;
     getLinkedUsersByCustomersIdAsync(fromId: string, toId: string): Promise<LinkedUsers | null>;
 }
