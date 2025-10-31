2c6cf708380db032d59a9114dee9bf0f0ee87de3
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-31 17:53:36 -0300

    refactor: add the parameter ipAddress in validateTokenAsync method

--- src/constants/contracts/auth/IAuthService.contract.ts
+++ src/constants/contracts/auth/IAuthService.contract.ts
@@ -5,6 +5,6 @@ import { ResetPasswordDto } from "src/dtos/reset_password/reset_password.dto";
 export interface IAuthService {
     signIn(auth: AuthRequestDto): Promise<AuthResponseDto>;
     recoverPassword(email: string, ipAddress: string): Promise<void>;
-    validateTokenAsync(token: string): Promise<void>;
+    validateTokenAsync(token: string, ipAddress: string): Promise<void>;
     resetPasswordAsync(resetPassword: ResetPasswordDto): Promise<void>;
 }
