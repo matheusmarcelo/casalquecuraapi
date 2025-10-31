b6586793de6c91bac4bacf26217d6c03c00aa97a
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-31 16:47:00 -0300

    refactor: add the methods validateTokenAsync and resetPasswordAsync

--- src/constants/contracts/auth/IAuthService.contract.ts
+++ src/constants/contracts/auth/IAuthService.contract.ts
@@ -1,7 +1,10 @@
 import { AuthRequestDto } from "src/dtos/auth/authRequest.dto";
 import { AuthResponseDto } from "src/dtos/auth/authResponse.dto";
+import { ResetPasswordDto } from "src/dtos/reset_password/reset_password.dto";
 
 export interface IAuthService {
     signIn(auth: AuthRequestDto): Promise<AuthResponseDto>;
     recoverPassword(email: string, ipAddress: string): Promise<void>;
+    validateTokenAsync(token: string): Promise<void>;
+    resetPasswordAsync(resetPassword: ResetPasswordDto): Promise<void>;
 }
