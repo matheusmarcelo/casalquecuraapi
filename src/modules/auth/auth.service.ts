3fcfbfe0f43b6e9869c5b9792bf61dd1ec19a81a
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-30 22:15:14 -0300

    feature: create the password reset proccess

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { IAuthService } from 'src/constants/contracts/auth/IAuthService.contract';
@@ -7,6 +7,8 @@ import { DITokensService } from 'src/constants/enums/DITokens/DITokens.enum';
 import { AuthRequestDto } from 'src/dtos/auth/authRequest.dto';
 import { compareSync as bcryptCompareSync } from 'bcrypt';
 import { AuthResponseDto } from 'src/dtos/auth/authResponse.dto';
+import { MailerService } from '../mailer/mailer.service';
+import { SendEmailDto } from 'src/dtos/mailer/mailer.dto';
 
 @Injectable()
 export class AuthService implements IAuthService {
@@ -16,6 +18,7 @@ export class AuthService implements IAuthService {
         private readonly customerService: ICustomerService,
         private readonly jwtService: JwtService,
         private readonly configService: ConfigService,
+        private readonly mailerService: MailerService,
     ) { }
 
     async signIn(auth: AuthRequestDto): Promise<AuthResponseDto> {
@@ -34,5 +37,28 @@ export class AuthService implements IAuthService {
             user_role: customer!.role as string,
             expiresIn: +this.configService.get<number>('JWT_EXPIRATION_TIME')!,
         }
+    }
+
+    async resetPassword(email: string): Promise<void> {
+        const customer = await this.customerService.getCustomerByEmail(email);
+
+        if (!customer) {
+            throw new HttpException('Customer not found', HttpStatus.NOT_FOUND);
+        }
+
+        const token = this.generateToken();
+
+        const sendEmail: SendEmailDto = {
+            from: { name: 'Matheus', address: 'matheusmarcelo314@gmail.com' },
+            recipients: [{ name: 'Marcelo', address: 'matheusmarcelo314@gmail.com' }],
+            subject: 'Reset password test',
+            html: `<p><strong>token: ${token}</strong></p>, não compartilhe seu token`
+        }
+
+        return await this.mailerService.sendEmail(sendEmail);
+    }
+
+    private generateToken(): number {
+        return Math.floor(100000 + Math.random() * 900000);
     }
 }
