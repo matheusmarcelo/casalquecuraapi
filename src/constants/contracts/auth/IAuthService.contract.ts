558123972c33bcde26dd44b9a139996b030a911e
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-10-30 22:59:59 -0300

    refactor: change the name resetPassword to recoverPassword and add the property ipAddress

--- src/constants/contracts/auth/IAuthService.contract.ts
+++ src/constants/contracts/auth/IAuthService.contract.ts
@@ -3,5 +3,5 @@ import { AuthResponseDto } from "src/dtos/auth/authResponse.dto";
 
 export interface IAuthService {
     signIn(auth: AuthRequestDto): Promise<AuthResponseDto>;
-    resetPassword(email: string): Promise<void>;
+    recoverPassword(email: string, ipAddress: string): Promise<void>;
 }
