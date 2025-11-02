502c1aa59f715809843231b9792add71a888a13b
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-11-02 17:54:13 -0300

    refactor: change the response for method getCustomerActivitiesAsync

--- src/constants/contracts/customer-activity/ICustomerActivitiesRepository.contract.ts
+++ src/constants/contracts/customer-activity/ICustomerActivitiesRepository.contract.ts
@@ -1,8 +1,9 @@
+import { Activity } from "src/entitites/activity/activity.entity";
 import { CustomerActivity } from "src/entitites/customer-activity/customer-activity.entity";
 
 export interface ICustomerActivityRepository {
     createCustomerActivityAsync(customer_activity: CustomerActivity): Promise<void>;
-    getCustomerActivitiesAsync(customerId: string): Promise<CustomerActivity[]>;
+    getCustomerActivitiesAsync(customerId: string): Promise<Activity[]>;
     getCustomerActivitiesByActivityIdAsync(activityId: string): Promise<CustomerActivity[]>;
     getCustomerActivityAsync(id: string): Promise<CustomerActivity | null>;
     deleteCustomerActivityAsync(id: string): Promise<void>;
