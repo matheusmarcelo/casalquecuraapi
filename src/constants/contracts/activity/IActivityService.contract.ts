2525721024d587b480f9dd3edbcf95962f6ab7c8
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-11-25 22:10:41 -0300

    feat: add report contracts

--- src/constants/contracts/activity/IActivityService.contract.ts
+++ src/constants/contracts/activity/IActivityService.contract.ts
@@ -5,6 +5,8 @@ import { DalyActivities } from "src/entitites/daly-activities/daly_activities.en
 import { IMonthActivitiesRepository } from "../month-activities/IMonthActivitiesRepository.contract";
 import { MonthActivitiesDto } from "src/dtos/month_activities/month_activities.dto";
 import { MonthActivities } from "src/entitites/mont-activities/month_activities.entity";
+import { ChartReportInterval } from "src/constants/enums/chartReport/chartReport.enum";
+import { ReportDto } from "src/dtos/chart_report/chart_report.dto";
 
 export interface IActivityService extends Omit<IActivityRepository, 'createActivityAsync' | 'updateActivityAsync'> {
     createActivityAsync(activityDto: ActivityDto): Promise<void>;
@@ -12,4 +14,5 @@ export interface IActivityService extends Omit<IActivityRepository, 'createActiv
     markActivityCompletedAsync(dalyActivityDto: DalyActivitiesDto): Promise<void>;
     getDalyActivitiesAsync(customerId: string): Promise<DalyActivities[]>;
     getMonthlyActivitiesAsync(customerId: string): Promise<MonthActivities[]>;
+    getCustomerReportAsync(customerId: string, days: ChartReportInterval): Promise<ReportDto>;
 }
