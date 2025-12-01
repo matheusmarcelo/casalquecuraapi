a23e7a1cbc85c3e50a8447f509c7cb75de88968e
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-11-30 21:19:38 -0300

    feat: create the relationship for linked users

--- src/entitites/customer-activity/customer-activity.entity.ts
+++ src/entitites/customer-activity/customer-activity.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Customer } from "../customer/customer.entity";
 import { Activity } from "../activity/activity.entity";
+import { LinkedUsers } from "../linked-users/linked_users.entity";
 
 @Entity({ name: 'user_activities' })
 export class CustomerActivity {
@@ -9,9 +10,13 @@ export class CustomerActivity {
 
     @ManyToOne(type => Customer, (customer) => customer.id)
     @JoinColumn({ name: 'user_id' })
-    customer: Customer;
+    customer?: Customer;
 
     @ManyToOne(type => Activity, (activity) => activity.id)
     @JoinColumn({ name: 'activity_id' })
     activity: Activity;
+
+    @ManyToOne(type => LinkedUsers, (linkedUsers) => linkedUsers.id)
+    @JoinColumn({ name: 'linked_user_id' })
+    linkedUserId?: LinkedUsers;
 }
