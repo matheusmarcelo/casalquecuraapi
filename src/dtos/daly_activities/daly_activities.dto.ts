230943c293a7323392dfc6d257122685aa933bf5
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-11-30 21:18:59 -0300

    refactor: add linked users id prop

--- src/dtos/daly_activities/daly_activities.dto.ts
+++ src/dtos/daly_activities/daly_activities.dto.ts
@@ -1,11 +1,15 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, ValidateIf } from "class-validator";
 
 export class DalyActivitiesDto {
     @IsString({ message: 'User id must be a string type' })
-    @IsNotEmpty({ message: 'User id must have a value' })
+    @ValidateIf((o) => !o.linked_users_id)
     userId: string;
 
     @IsString({ message: 'Activity id must be a string type' })
     @IsNotEmpty({ message: 'Activity id must have a value' })
     activityId: string;
+
+    @ValidateIf((o) => !o.userId)
+    @IsString({ message: 'Linked users id must be string type' })
+    linked_users_id?: string;
 }
