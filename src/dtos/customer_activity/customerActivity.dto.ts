54647e67078b169736bb8df4c69608fb3d76db21
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-11-30 21:18:37 -0300

    refactor: add linked user id prop and validations

--- src/dtos/customer_activity/customerActivity.dto.ts
+++ src/dtos/customer_activity/customerActivity.dto.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString, ValidateIf, } from "class-validator";
 
 export class CustomerActivityDto {
+    @ValidateIf((o) => !o.linked_users_id)
     @IsString({ message: 'Customer id must be string type' })
-    @IsNotEmpty({ message: 'Customer id must have a value' })
-    customer_id: string;
+    customer_id?: string;
+
+    @ValidateIf((o) => !o.customer_id)
+    @IsString({ message: 'Linked users id must be string type' })
+    linked_users_id?: string;
 
     @IsString({ message: 'Activity id must be string type' })
     @IsNotEmpty({ message: 'Activity id must have a value' })
