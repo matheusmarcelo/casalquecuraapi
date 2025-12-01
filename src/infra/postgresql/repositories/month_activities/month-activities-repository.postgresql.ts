1be4aaefa00759beb255d781f221738475689a08
Matheus Marcelo <matheusmarcelo314@gmail.com>
2025-11-30 21:21:37 -0300

    refactor: refactor methods for get data to linked users

--- src/infra/postgresql/repositories/month_activities/month-activities-repository.postgresql.ts
+++ src/infra/postgresql/repositories/month_activities/month-activities-repository.postgresql.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { IMonthActivitiesRepository } from 'src/constants/contracts/month-activities/IMonthActivitiesRepository.contract';
 import { DalyActivities } from 'src/entitites/daly-activities/daly_activities.entity';
 import { MonthActivities } from 'src/entitites/mont-activities/month_activities.entity';
+import { NumericHelper } from 'src/helpers/numeric.helpers';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -23,30 +24,57 @@ export class MonthActivitiesRepositoryPostgresql implements IMonthActivitiesRepo
         await this.monthActivityRepository.update(id, { totalScore });
     }
 
-    async getMonthActivityAsync(customerId: string, month: number, year: number): Promise<MonthActivities | null> {
-        return await this.monthActivityRepository.findOne({
-            where: {
-                user: { id: customerId },
-                month,
-                year
-            }
-        });
+    async getMonthActivityAsync(month: number, year: number, customerOrLinkedUserId?: string): Promise<MonthActivities | null> {
+        const isNumeric = new NumericHelper().isNumeric(customerOrLinkedUserId || '0');
+        if (isNumeric) {
+            return await this.monthActivityRepository.findOne({
+                where: {
+                    linkedUserId: { id: customerOrLinkedUserId },
+                    month,
+                    year
+                }
+            });
+
+        } else {
+            return await this.monthActivityRepository.findOne({
+                where: {
+                    user: { id: customerOrLinkedUserId },
+                    month,
+                    year
+                }
+            });
+        }
     }
 
-    async getMonthlyActivitiesAsync(customerId: string): Promise<MonthActivities[]> {
-        return await this.monthActivityRepository.find({ where: { user: { id: customerId } } });
+    async getMonthlyActivitiesAsync(customerOrLinkedUserId: string): Promise<MonthActivities[]> {
+        const isNumeric = new NumericHelper().isNumeric(customerOrLinkedUserId);
+        if (isNumeric) {
+            return await this.monthActivityRepository.find({
+                where: { user: { id: customerOrLinkedUserId } }
+            });
+        } else {
+            return await this.monthActivityRepository.find({
+                where: { linkedUserId: { id: customerOrLinkedUserId } }
+            });
+        }
     }
 
-    async getTotalMonthActivitiesAsync(customerId: string): Promise<number> {
-        const totalActivitiesThisMonth = await this.dalyActivityRepository
-            .createQueryBuilder('activity')
-            .where('activity.user_id = :userId', { userId: customerId })
-            .andWhere(
-                `activity.completion_date BETWEEN 
+    async getTotalMonthActivitiesAsync(customerOrLinkedUserId: string): Promise<number> {
+        const isNumeric = new NumericHelper().isNumeric(customerOrLinkedUserId);
+        const totalActivitiesThisMonth = this.dalyActivityRepository
+            .createQueryBuilder('activity');
+
+        if (isNumeric) {
+            totalActivitiesThisMonth.where('activity.linked_user_id = :userId', { userId: customerOrLinkedUserId });
+        } else {
+            totalActivitiesThisMonth.where('activity.user_id = :userId', { userId: customerOrLinkedUserId });
+        }
+
+        const data = await totalActivitiesThisMonth.andWhere(
+            `activity.completion_date BETWEEN 
             date_trunc('month', CURRENT_DATE) AND CURRENT_TIMESTAMP`
-            )
-            .getCount();
+        ).getCount();
 
-        return totalActivitiesThisMonth || 0;
+        return data || 0;
     }
-}
+} 
